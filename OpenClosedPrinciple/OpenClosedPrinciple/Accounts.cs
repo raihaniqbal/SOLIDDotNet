fda3d1c10fb076fea8b3bfe39d1eb881599e9a78
Raihan Iqbal <raihan.iqbal@gmail.com>
2020-07-21 16:49:18 +1000

    First commit with intial version of OpenClosed Principle Demo

---
+++ OpenClosedPrinciple/OpenClosedPrinciple/Accounts.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OpenClosedPrinciple
+{
+    public class Accounts
+    {
+        private const string _emailDomain = "acme.com";
+        public static Employee CreateEmployee(Applicant applicant)
+        {
+            return new Employee
+            {
+                FirstName = applicant.FirstName,
+                LastName = applicant.LastName,
+                Email = String.Format("{0}{1}", applicant.Email.Substring(0, applicant.Email.IndexOf('@') + 1), _emailDomain)
+            };
+        }
+    }
+}
---
+++ OpenClosedPrinciple/OpenClosedPrinciple/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OpenClosedPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<Applicant> applicants = new List<Applicant>();
+            applicants.Add(new Applicant { FirstName = "Raihan", LastName = "Iqbal", Email = "raihan.iqbal@gmail.com" });
+            applicants.Add(new Applicant { FirstName = "John", LastName = "Doe", Email = "john@random.com" });
+
+            foreach(var applicant in applicants)
+            {
+                var employee = Accounts.CreateEmployee(applicant);
+                Console.WriteLine(String.Format("First Name: {0}, Last Name: {1}, Email: {2}", employee.FirstName, employee.LastName, employee.Email));
+            }
+        }
+    }
+}
