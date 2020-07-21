dbbaf36bb91185934a989036863df140a75adfb9
Raihan Iqbal <raihan.iqbal@gmail.com>
2020-07-22 09:17:24 +1000

    Added demo code that breaks the Liskov Principle

---
+++ LiskovPrinciple/LiskovPrinciple/Manager.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LiskovPrinciple
+{
+    public class Manager : Employee
+    {
+        public override void CalculateSalary(int rank)
+        {
+            decimal baseAmount = 14.50M;
+
+            Salary = baseAmount + (rank * 4);
+        }
+
+        public virtual void GeneratePerformanceReview()
+        {
+            Console.WriteLine("I'm doing a performance review for someone I manage");
+        }
+    }
+}
---
+++ LiskovPrinciple/LiskovPrinciple/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace LiskovPrinciple
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new Manager();
+            manager.FirstName = "Emma";
+            manager.LastName = "Stone";
+            manager.CalculateSalary(5);
+            
+            Employee employee = new Employee();
+            employee.FirstName = "John";
+            employee.LastName = "Doe";
+            employee.CalculateSalary(1);
+            employee.AssignManager(manager);
+
+            Console.WriteLine(String.Format("{0} {1} your hourly rate is: ${2}", employee.FirstName, employee.LastName, employee.Salary));
+
+            /**
+             * The following code breaks the Liskov Principle since I am not able to replace and instance of Employee with 
+             * an instance of it's sub-type CEO. 
+             */
+            //Employee ceo = new CEO();
+            //ceo.FirstName = "Raihan";
+            //ceo.LastName = "Iqbal";
+            //ceo.CalculateSalary(10);
+            //ceo.AssignManager(manager); // this will throw an exception
+        }
+    }
+}
