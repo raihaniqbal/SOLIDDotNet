32aa60e898d6e3de8fd20fe4942b90710f95ba03
Raihan Iqbal <raihan.iqbal@gmail.com>
2020-07-22 10:48:01 +1000

    Added code that violates Interface Segregation Principle

---
+++ InterfaceSegregationPrinciple/InterfaceSegregationPrinciple/Implementations/Book.cs
@@ -0,0 +1,29 @@
+﻿using InterfaceSegregationPrinciple.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceSegregationPrinciple.Implementations
+{
+    public class Book : ILibraryItem
+    {
+        public string LibraryId { get; set; }
+        public string Title { get; set; }
+        public string Author { get; set; }
+        public int Pages { get; set; }
+        public DateTime? BorrowDate { get; set; }
+        public string Borrower { get; set; }
+
+        public void CheckIn()
+        {
+            Borrower = "";
+            BorrowDate = null;
+        }
+
+        public void CheckOut(string borrower)
+        {
+            Borrower = borrower;
+            BorrowDate = DateTime.Today;
+        }
+    }
+}
---
+++ InterfaceSegregationPrinciple/InterfaceSegregationPrinciple/Interfaces/ILibraryItem.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace InterfaceSegregationPrinciple.Interfaces
+{
+    public interface ILibraryItem
+    {
+        string LibraryId { get; set; }
+        string Title { get; set; }
+        string Author { get; set; }
+        int Pages { get; set; }
+        DateTime? BorrowDate { get; set; }
+        string Borrower { get; set; }
+        void CheckIn();
+        void CheckOut(string borrower);
+
+    }
+}
